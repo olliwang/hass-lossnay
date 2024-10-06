82d3383033281c9c22d6d78518c90cffa1afdfac
Olli Wang <olliwang@ollix.com>
2024-10-06 21:43:41 +0800

    Implements the dummy fan.

--- __init__.py
+++ __init__.py
@@ -1,35 +1,18 @@
 """The Lossnay integration."""
 
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.LIGHT]
+from .const import DOMAIN
 
-# TODO Create ConfigEntry type alias with API object
-# TODO Rename type alias and update all entry annotations
-type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
+PLATFORMS: list[Platform] = [Platform.FAN]
 
-
-# TODO Update entry annotation
-async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Lossnay from a config entry."""
-
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
-
-
-# TODO Update entry annotation
-async def async_unload_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
---
+++ fan.py
@@ -0,0 +1,108 @@
+"""Platform for fan integration."""
+from homeassistant.components.fan import (
+    FanEntity,
+    FanEntityFeature,
+)
+
+from typing import Any, Optional
+
+# import adafruit_mcp4725
+# import busio
+# import board
+
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Blueair fans from config entry."""
+    async_add_entities([LossnayFan()])
+
+async def async_remove_entry(hass, entry):
+    """Handle removal of an entry."""
+    # Perform necessary cleanup actions
+    if DOMAIN in hass.data:
+        del hass.data[DOMAIN]
+
+class LossnayFan(FanEntity):
+    """Controls Fan."""
+
+    def __init__(self):
+        """Initialize the temperature sensor."""
+        self._is_on = False
+        self._max_speed = 4
+        self._name = "Lossnay Ventilation"
+        self._percentage = None
+        self._speed_step = 100 / self._max_speed
+        self._unique_id = "lossnay_ventilation"
+
+    async def async_set_percentage(self, percentage: int) -> None:
+        """Sets fan speed percentage."""
+        self._percentage = percentage
+        self.update_fan_speed()
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the fan off."""
+        self._is_on = False
+        self._percentage = None
+        self.async_write_ha_state()
+
+    async def async_turn_on(self, percentage, preset_mode, **kwargs: Any):
+        self._is_on = True
+
+        if percentage is not None:
+            self._percentage = percentage
+        elif self._percentage is None:
+            self._percentage = 25
+
+        self.async_write_ha_state()
+
+    @property
+    def is_on(self) -> int:
+        return self._is_on
+
+    @property
+    def name(self):
+        """Return the name of the fan."""
+        return self._name
+
+    @property
+    def percentage(self) -> Optional[int]:
+        """Return the current speed percentage."""
+        if self._percentage is None:
+            self._percentage = self._speed_step
+        return self._percentage
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
+
+    @property
+    def speed_count(self) -> int:
+        """Return the number of speeds the fan supports."""
+        return self._max_speed
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return FanEntityFeature.SET_SPEED | FanEntityFeature.TURN_ON | FanEntityFeature.TURN_OFF
+
+    @property
+    def unique_id(self):
+        """Return the unique ID of the fan."""
+        return self._unique_id
+
+    def update_fan_speed(self):
+        half_speed_step = self._speed_step / 2
+        speed = 0
+        percentage = 0
+        while True:
+            if abs(self._percentage - percentage) <= half_speed_step:
+                break
+            percentage += self._speed_step
+            speed += 1
+            if speed == self._max_speed:
+                break
+
+        self._percentage = percentage
